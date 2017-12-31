68883bfce4bd1b62f8f5cad8cc0f8ef47326922b
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-12-31 04:46:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,4 +24,12 @@ class Student
     DB[:conn].execute("DROP TABLE students;")
   end
 
+  def self.save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?,?);
+      SQL
+    DB[:conn].execute(sql, @name, @grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students;")[0][0]
+
 end
